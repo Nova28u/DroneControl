9069416a33f05899f28e9f79897f84c6b67b24c5
FYSword <30581818+FYSword@users.noreply.github.com>
2017-07-31 12:51:13 +0800

    Update main.c
    Total overhaul

--- main.c
+++ main.c
@@ -9,50 +9,109 @@ int playsound2(void);
 
 //message functions declare here later
 void main(void) {
+  
     
 ADCON1 = 0x0F;         //allow usage of digital inputs for all pins
-TRISA = 0b00111111;    //Declare extra output pins for LCD
+TRISA = 0b00111111;    //Declare extra output pins for LCD 
 TRISB = 0b11001111;    //Declare inputs from RB0 to RB3 and output from RB4 and RB5 ignore RB6 and RB7 note: RB5 is speaker
 TRISC = 0b00000000;    //Declare output pins for LCD
 TRISD = 0b00000000;    //Declare outputs for 7 segment
-TRISE = 0b00000000;   //LCD selector
-char v1,v2,v3,v4;
-
+TRISE = 0b00000000;    //LCD selector
+char autodecision,tiltangle1,direction;
+int invalid1,invalid2,invalid3;
 
 while(1){
-
-v1= getkeypad();
-
-if(v1=='c'){
     
-    //"input tilt simulation 90 max"
-    v2= getkeypad();
-    //playsound1
-    //decrement tilt to zero
-    //playsound2
-   
-}//if
+ 
+    do{  
+    
+    invalid1=0;
+    // "C autopilot
+    // D to turn"
 
-else{
-   
-   //"select turn direction
-   v3= getkeypad();
-
-   switch(v3){
+    autodecision=getkeypad();
         
-       case 'A': //"turning right"
-                 //display turn angle
-           
-       break;
+    if(autodecision=='C'){
+    
+        //"input tilt 
+        // 90 max"
+    
+        tiltangle1= getkeypad();
+    
+        //"A=left
+        //B=right"
+    
+        direction= getkeypad();
+  
+        do{
+            invalid2=0;
+            
+            if(direction=='A'){
+            //"turning left"
+            //display tilt angle
+            }
+
+            else if(direction=='B'){
+            //"turning right"
+            //display tilt angle
+            }
+            else{
+            //"invalid input"
+                 invalid2=1;
+            } 
+        }while(invalid2==1);   
+    
+        //playsound1
+        
+        //decrement tiltangle1 to zero
+        
+        //playsound2
+    
+
+        }//if1
+
+    else if(autodecision=='D'){
+   
+        //"A=left
+        //B=right"
+        direction= getkeypad();
+        
+        do{ 
+        
+        invalid3=0;    
+        
        
-       case 'B': //"Turing left"
-                 //display turn angle
+            if(direction=='B'){ //"turning right"
+                 //display turn angle 15 
+            }
            
-           break;
+    
+       
+            else if(direction=='A'){ //"Turing left"
+                 //display turn angle 15
+            }
+           
+            
+       
+                else{//"invalid input"
+                    invalid3=1;}
+                    
+        
+        }while(invalid3==1); //do while loop to check direction for manual turn
+                
+        
+    }//if2
 
+    else{
+        //"invalid input"
+        invalid1=1;
+    }
 
-}//switch
+        }while(invalid1==1); //check autodecision for auto or not and loop again if invalid
 
-}//while
+}//large while 
 
 }//main
+
+
+
