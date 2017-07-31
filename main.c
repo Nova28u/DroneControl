fb1f5c6d811bb8097ef4818590ff5baaa9a65282
FYSword <30581818+FYSword@users.noreply.github.com>
2017-07-31 09:53:44 +0800

    Update main.c
    remove one spacing

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ TRISA = 0b00111111;    //Declare extra output pins for LCD
 TRISB = 0b11001111;    //Declare inputs from RB0 to RB3 and output from RB4 and RB5 ignore RB6 and RB7 note: RB5 is speaker
 TRISC = 0b00000000;    //Declare output pins for LCD
 TRISD = 0b00000000;    //Declare outputs for 7 segment
-TRISE = 0b0000 0000;   //LCD selector
+TRISE = 0b00000000;   //LCD selector
 char keypad_input;
 
 //"press C to fly straight"
