3bcb3b877368122e95a8664e69f3045709ac8142
FYSword <30581818+FYSword@users.noreply.github.com>
2017-07-31 10:28:21 +0800

    Update main.c
    main skeleton added

--- main.c
+++ main.c
@@ -16,25 +16,43 @@ TRISB = 0b11001111;    //Declare inputs from RB0 to RB3 and output from RB4 and 
 TRISC = 0b00000000;    //Declare output pins for LCD
 TRISD = 0b00000000;    //Declare outputs for 7 segment
 TRISE = 0b00000000;   //LCD selector
-char keypad_input;
-
-//"press C to fly straight"
-
-int getkeypad()
+char v1,v2,v3,v4;
 
 
+while(1){
 
+v1= getkeypad();
 
-
-
-
-
-
-
-
-
-
+if(v1=='c'){
     
-}   
-    
- 
+    //"input tilt simulation 90 max"
+    v2= getkeypad();
+    //playsound1
+    //decrement tilt to zero
+    //playsound2
+   
+}//if
+
+else{
+   
+   //"select turn direction
+   v3= getkeypad();
+
+   switch(v3){
+        
+       case 'A': //"turning right"
+                 //display turn angle
+           
+       break;
+       
+       case 'B': //"Turing left"
+                 //display turn angle
+           
+           break;
+
+
+}//switch
+
+}//while
+
+}//main
