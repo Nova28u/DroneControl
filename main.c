4bdce2290c3c660d6529bd4e5dcb2dde9de709cc
FYSword <30581818+FYSword@users.noreply.github.com>
2017-08-07 12:05:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-#include "header.h"
+#include <xc.h>
+#include "main_project.h"
 #define DA PORTDbits.RD0 // Define data available from encoder
 
 #define LCD_DATA PORTA
-#define LCD_RS 1PORTAbits.RA0
+#define LCD_RS PORTAbits.RA0
 #define LCD_E PORTBbits.RB1
 #define SET 0b00111000
 #define D1C1 0b00001100
@@ -10,21 +11,10 @@
 #define CLEAR 0b00000001
 #define HOME 0b00000010//for LCD
 
-
-char MESS[][32] = {
-				{"PRESS C TO FLY STRAIGHT"},
-				{"INPUT TILT SIMULATION"},
-				{"TILT CORRECTED"},
-				{"SELECT TURN DIRECTION"},
-				{"TURNING RIGHT"},
-				{"TURNING LEFT"},
-				{"INVALID INPUT"}
-				};
-
-
 char getkeypad(void);
-void safeteysound(void);
-void warningsound(void);//Unneeded function declaration
+void safetysound(void);
+void warningsound(void);
+void sevensegment(char autodecision);
 
 void main(void) {
   
@@ -133,35 +123,41 @@ while(1){
 
 char getkeypad(void)
 {
- char value_needed[]={'1','2','3','F','4','5','6','E','7','8','9','D','A','0','B','c' }; //look up table for inputs to encoder, index is binary expression
+ char value_needed[]={'1','2','3','F','4','5','6','E','7','8','9','D','A','0','B','C' }; //look up table for inputs to encoder, index is binary expression
 // char value_used; //value used from look up table
- int selection,selection2; //Declare for variable needed to perform lookup
+ char selection,selection2; //Declare for variable needed to perform lookup
  char value_used;
  
 
- while(1){ //loop scanning process
- 
 if(DA==1)
-{   _delay(80000); //80ms delay to act as software debounce, also assists physical debounce circuit    
-    selection= (PORTC & 0x0F); //Selection is the variable used to performed lookup, PORTC in binary expression also preserves inputs for rc0 to rc3. This statement reads input.
+{      
+    selection= (PORTB & 0x0F); //Selection is the variable used to performed lookup, PORTC in binary expression also preserves inputs for rc0 to rc3. This statement reads input.
    _delay(80000);
-   selection2= (PORTC & 0x0F);
+   selection2= (PORTB & 0x0F);
    if(selection2==selection)
    {
     value_used= value_needed[selection]; //convert binary input into character through look up table
    }
-   return value_used;
+
    
 } //Execute if data available is 1
 
-    
-   } 
  
   return value_used;
 } //keypad     
+
   
 
-
+char MESS[][32] = {
+				{"PRESS C TO FLY STRAIGHT"},
+				{"INPUT TILT SIMULATION"},
+				{"TILT CORRECTED"},
+				{"SELECT TURN DIRECTION"},
+				{"TURNING RIGHT"},
+				{"TURNING LEFT"},
+				{"INVALID INPUT"}
+				};
+/*
 void lcdmessage()
 {
 	unsigned char i;
@@ -185,53 +181,53 @@ void lcdmessage()
     
     
 	m= PORTC & 0xf;
-	TRISC=m;
+	PORTC=m;
 	for(i=0;MESS[m][i]!=0;i++)
 		LCD_sendData(MESS[m][i]);
 }
 
 
-void LCD_sendCW(char e)
+void LCD_sendCW(char x)
 {
 	LCD_RS=0;
 	LCD_E=1;
-	LCD_DATA=CW[e];
+	LCD_DATA=x;
 	LCD_E=0;
 	_delay(1000);
 }
 
-void LCD_sendData(char e)
+void LCD_sendData(char x)
 {
 	LCD_RS=0;
 	LCD_E=1;
-	LCD_DATA=e;
+	LCD_DATA=x;
 	LCD_E=0;
 	_delay(500);
 }
-
-sevensegment(char autodecision)
+*/
+void sevensegment(char autodecision)
 {
-int lcd[10] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000,0b1111111,0b1111011};   //CC lookup table
-int DEGREE = 0, HN = 0, LN = 0;
+int display[10] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000,0b1111111,0b1111011};   //CC lookup table
+int  HN = 0, LN = 0;
 int t;
 
 	while(1)
 		{
 			if(autodecision=='C')
 				{
-				LN = DEGREE%10; //Finding the Lower Number
-				HN = DEGREE/10; //Finding the Higher Number
+				HN = getkeypad(); //Finding the Lower Number
+				LN = getkeypad(); //Finding the Higher Number
 
 				for(t=0;t<1000;t++)
 					{
 						PORTE = 0b00000001;
-						PORTD = lcd[LN];  //writing to LCD
+						PORTD = display[LN];  //writing to LCD
 						_delay(500); //setting time delay so we can see the lower number in a long enough time
 					}
 				for(t=0;t<1000;t++)
 					{
 						PORTE = 0b00000010;
-						PORTD = lcd[HN]; //writing to LCD
+						PORTD = display[HN]; //writing to LCD
 						_delay(500); //setting time delay so we can see the higher number in a long enough time
 					}
 				LN = 0;
@@ -239,27 +235,27 @@ int t;
 				}
 			else if(autodecision=='D')
 				{
-				while(DEGREE>0)
+				while((HN*10+LN)>0)
 					{
 					for(t=0;t<1000;t++)
 					{
-						LN = DEGREE%10; //Finding the Lower Number
-						HN = DEGREE/10; //Finding the Higher Number
+						HN = getkeypad(); //Finding the Lower Number
+						LN = getkeypad(); //Finding the Higher Number
 						
 						PORTE = 0b00000001;
-						PORTD = lcd[LN];  //writing to LCD
+						PORTD = display[LN];  //writing to LCD
 						_delay(500); //setting time delay so we can see the lower number in a long enough time
 						
 						PORTE = 0b00000010;
-						PORTD = lcd[HN]; //writing to LCD
+						PORTD = display[HN]; //writing to LCD
 						_delay(500); //setting time delay so we can see the higher number in a long enough time
 						
-						DEGREE/1.1;
+						(HN*10+LN)/1.1;
 					}
 					}
 				}
 	}
-return autodecision;
+//return autodecision;
 }// end of seven segment
 
 
@@ -267,31 +263,20 @@ void safetysound(void){
 int x;
 for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
             _delay(758);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(758);
         }
         for(x=0;x<1500;x++)
         {
-        PORTAbits.RA4=0;
+        PORTBbits.RB5=0;
         }
           for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
             _delay(758);
-            PORTAbits.RA4=0;
-            _delay(758);
-        }
-        for(x=0;x<3000;x++)
-        {
-        PORTAbits.RA4=0;
-        }
-        for (x=0;x<20;x++)
-        {
-            PORTAbits.RA4=1;
-            _delay(758);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(758);
         }
         for(x=0;x<3000;x++)
@@ -300,9 +285,20 @@ for (x=0;x<20;x++)
         }
         for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
+            _delay(758);
+            PORTBbits.RB5=0;
+            _delay(758);
+        }
+        for(x=0;x<3000;x++)
+        {
+        PORTBbits.RB5=0;
+        }
+        for (x=0;x<20;x++)
+        {
+            PORTBbits.RB5=1;
             _delay(980);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(980);
         }
         for(x=0;x<1000;x++)
@@ -311,9 +307,9 @@ for (x=0;x<20;x++)
         }
         for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
             _delay(758);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(758);
         }
         for(x=0;x<3000;x++)
@@ -322,25 +318,25 @@ for (x=0;x<20;x++)
         }
         for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
             _delay(649);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(649);
         }
          for(x=0;x<5500;x++)
         {
-        PORTAbits.RA4=0;
+        PORTBbits.RB5=0;
         }
         for (x=0;x<20;x++)
         {
-            PORTAbits.RA4=1;
+            PORTBbits.RB5=1;
             _delay(1316);
-            PORTAbits.RA4=0;
+            PORTBbits.RB5=0;
             _delay(1316);
         }
          for(x=0;x<5750;x++)
         {
-        PORTAbits.RA4=0;
+        PORTBbits.RB5=0;
         }
 }//end of sound
 
@@ -348,19 +344,17 @@ void warningsound(void){
     int x;
 for (x=0;x<200;x++)
         {
-            PORTEbits.RE0=1;
+            PORTBbits.RB5=1;
             _delay(568);
-            PORTEbits.RE0=0;
+            PORTBbits.RB5=0;
             _delay(269);
         }
           for (x=0;x<600;x++)
         {
-            PORTEbits.RE0=1;
+            PORTBbits.RB5=1;
             _delay(239);
-            PORTEbits.RE0=0;
+            PORTBbits.RB5=0;
             _delay(239);
         }
 }//end of sound
-
-
 
